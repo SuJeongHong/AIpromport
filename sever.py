8621688290e3a0891b59e53aae3c82a7ed296a93
Cree <sujenghong@gmail.com>
2024-05-23 11:43:56 +0900

    location APIconnect succes

--- sever.py
+++ sever.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 from test_retriever import responsetext
 
 app = Flask(__name__)
+CORS(app)  # 모든 도메인에 대해 CORS 허용
 
 @app.route('/')
 def index():
@@ -9,20 +11,29 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    user_query = request.form['query']  # 사용자의 질문을 받아옴
+    data = request.get_json()  # 요청 데이터를 JSON 형식으로 파싱
+    if data is None:
+        return jsonify({'error': 'No data provided'}), 400
+
+    user_query = data.get('query')  # 사용자의 질문을 받아옴
+    user_location = data.get('location')  # 사용자의 위치를 받아옴
+
+    if not user_query or not user_location:
+        return jsonify({'error': 'Invalid data provided'}), 400
 
     # 리트리버를 통해 사용자의 질문 처리하고 응답 생성
-    response_text = process_query(user_query)
+    response_text = process_query(user_query, (user_location['lat'], user_location['lon']))
 
     # 응답을 JSON 형식으로 반환
     return jsonify({'query': user_query, 'response': response_text})
 
-def process_query(query):
+def process_query(query, user_location):
     # 리트리버 함수를 호출하여 사용자의 질문에 대한 정보를 가져옵니다.
-    relevant_place_info = responsetext(query)
+    relevant_place_info = responsetext(query, user_location)
 
     # 리트리버에서 반환된 정보를 출력합니다.
     print("Retriever Response:", relevant_place_info)
+    print("location : ", user_location)
 
     # 리트리버에서 가져온 정보를 응답으로 사용합니다.
     response_text = relevant_place_info
--- test_retriever.py
+++ test_retriever.py
@@ -9,27 +9,20 @@ import json
 import math
 from sklearn.metrics.pairwise import cosine_similarity
 
-
-
 os.environ['OPENAI_API_KEY'] = "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"
 
 # JSON 파일 경로
 json_file_path = "restaurant.json"
 vector_data_file_path = "vector_data.json"
 
-# 사용자 위치 좌표
-user_location = (37.534274, 126.970481)
-
-
 def get_nearest_places(user_location, places, n):
     places_with_distance = [(place["id"], calculate_distance(user_location, place["location"])) for place in places]
     sorted_places = sorted(places_with_distance, key=lambda x: x[1])
     return [place for place, distance in sorted_places[:n]]
 
-
 def calculate_distance(coord1, coord2):
-    lat1, lon1 = coord1
-    lat2, lon2 = coord2
+    lat1, lon1 = coord1  # (lat, lon) 형식의 튜플로 가정
+    lat2, lon2 = coord2  # (lat, lon) 형식의 리스트로 가정
     R = 6371  # 지구의 반지름 (단위: km)
     dlat = math.radians(lat2 - lat1)
     dlon = math.radians(lon2 - lon1)
@@ -39,33 +32,19 @@ def calculate_distance(coord1, coord2):
     distance = R * c
     return distance
 
-
 with open(json_file_path, 'r', encoding='utf-8') as file:
     json_places = json.load(file)
 
 with open(vector_data_file_path, 'rt', encoding='utf-8') as file:
     vector_data = json.load(file)
 
-nearest_place_ids_int = get_nearest_places(user_location, json_places, n=10)
-nearest_place_ids = [str(item) for item in nearest_place_ids_int]
-
-vector_data_ids = list(vector_data.keys())
-
-small_db = {}
-for i in range(len(nearest_place_ids)):
-    if nearest_place_ids[i] in vector_data_ids:
-        small_db[nearest_place_ids[i]] = vector_data[nearest_place_ids[i]]["embedding"]
-
 embeddings = OpenAIEmbeddings(api_key=os.environ['OPENAI_API_KEY'], model="text-embedding-3-small")
-
 
 def embed_user_input(user_input):
     embedding = embeddings.embed_query(user_input)
     return embedding
 
-
 def retrieve_from_vector_database(user_input):
-    # 사용자 입력에 가장 가까운 장소 찾기
     k = 5
     user_embedding = embed_user_input(user_input)
     top_places = [None] * (k + 1)
@@ -75,8 +54,6 @@ def retrieve_from_vector_database(user_input):
     for place_id, value in small_db.items():
         if value is not None:
             similarity = cosine_similarity([user_embedding], [value])[0][0]
-
-            # k개 반환
             if similarity > min(top_similarities):
                 min_similarity_index = top_similarities.index(min(top_similarities))
                 if len(top_similarities) < k:
@@ -85,11 +62,6 @@ def retrieve_from_vector_database(user_input):
                 else:
                     top_places[min_similarity_index] = place_id
                     top_similarities[min_similarity_index] = similarity
-            # 2개 반환
-            # if similarity > min(top_similarities):
-            #     index_to_replace = top_similarities.index(min(top_similarities))
-            #     top_places[index_to_replace] = place_id
-            #     top_similarities[index_to_replace] = similarity
 
     for place_id in top_places:
         similar_place = next((p for p in json_places if str(p["id"]) == str(place_id)), None)
@@ -97,28 +69,22 @@ def retrieve_from_vector_database(user_input):
 
     context = ""
     if most_similar_places:
-        i = 0
-        for place in most_similar_places[:-1]:
-            i += 1
+        for i, place in enumerate(most_similar_places[:-1]):
             name = place["name"]
             type = place["type"]
             address = place["address"]
             rating = place["rating"]
             tags = ", ".join(place["tag"])
             details = ", ".join(place["detail"])
-            # 사용자 입력과 관련된 문맥 생성
             context += f'''장소 이름은 '{name}'고 장소 타입은 '{type}'이다. 
-    데이터베이스 내의 장소 중 현재 위치와 {i}번째로 가까운 장소이며, 주소는 '{address}'이고 평점은 '{rating}'점이다. 
+    데이터베이스 내의 장소 중 현재 위치와 {i+1}번째로 가까운 장소이며, 주소는 '{address}'이고 평점은 '{rating}'점이다. 
     세부사항으로는 '{tags}', '{details}' 등이 있다.\n'''
     else:
         return "해당 장소 정보를 찾을 수 없습니다."
 
     context += f"\n사용자 입력: {user_input}"
-    #print(context)
     return context
 
-
-# Langchain 설정
 template = """Answer the question based only on the following context:
 
 {context}
@@ -135,14 +101,14 @@ chain = (
         | StrOutputParser()
 )
 
-#user_input = input("사용자 입력을 입력하세요: ")
-#user_input = "소개팅하기 괜찮은 장소 추천해줘"
-#result = chain.invoke(user_input)
-#print("질문 >>", user_input)
-#print(result)
+def responsetext(user_input, user_location):
+    def update_small_db(user_location):
+        global small_db
+        nearest_place_ids_int = get_nearest_places(user_location, json_places, n=10)
+        nearest_place_ids = [str(item) for item in nearest_place_ids_int]
+        vector_data_ids = list(vector_data.keys())
+        small_db = {place_id: vector_data[place_id]["embedding"] for place_id in nearest_place_ids if place_id in vector_data_ids}
 
-def responsetext(user_input):
+    update_small_db(user_location)
     result = chain.invoke(user_input)
     return result
-
-
