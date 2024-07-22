3b8bc6e22eb576e3b4969a7b166f217c05e02e3a
Cree <sujenghong@gmail.com>
2024-07-22 14:16:25 +0900

    mongodb_연결완료

--- sever.py
+++ sever.py
@@ -19,7 +19,7 @@ def search():
         longitude = user_location.get('lon')
 
         # 사용자 위치 좌표를 튜플로 만듦
-        location = (latitude, longitude)  # (37.5504, 126.9601)
+        location = (37.532,126.971)#(latitude, longitude)
 
         # 리트리버를 통해 사용자의 질문 처리하고 응답 생성
         response_text = process_query(user_query, location)
--- test_retriever.py
+++ test_retriever.py
@@ -4,16 +4,21 @@ from langchain.schema import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 import os
-import json
 import math
+import requests
+from pymongo import MongoClient
 from sklearn.metrics.pairwise import cosine_similarity
 
-# 직접 API 키 설정
+# API 키 설정
 api_key = "sk-proj-pyCLfEK2A4Ju3Jx7eS1VT3BlbkFJxM4iGvvDjEeZ78gZerP4"
+# MongoDB key
+connection_string = "mongodb+srv://hae081128:1213@test1.fe6gacs.mongodb.net/"
 
-# JSON 파일 경로
-json_file_path = "korea_rest_update.json"
-vector_data_file_path = "Untitled.json"
+# MongoDB 서버에 연결
+client = MongoClient(connection_string)
+db = client['korea_rest']
+cc_r = db['Seoul_rest']  # 장소 데이터
+cc_v = db['Seoul_vector']  # 벡터 데이터
 
 def get_address_from_coordinates(lat, lon):
     api_key = '61d4dab14a5504fb5a190597a427da0d'
@@ -43,6 +48,7 @@ def get_address_from_coordinates(lat, lon):
 
     return None, "No address found"
 
+# knn 함수: 사용자의 위치에서 가까운 n개의 위치
 def get_nearest_places(user_location, places, n):
     places_with_distance = [(place["id"], calculate_distance(user_location, place["location"])) for place in places]
     sorted_places = sorted(places_with_distance, key=lambda x: x[1])
@@ -53,18 +59,12 @@ def calculate_distance(coord1, coord2):
     lat2, lon2 = coord2
     R = 6371  # 지구의 반지름 (단위: km)
     dlat = math.radians(lat2 - lat1)
-    dlon = math.radians(lat2 - lon1)
+    dlon = math.radians(lon2 - lon1)
     a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) \
         * math.cos(math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     distance = R * c
     return distance
-
-with open(json_file_path, 'r', encoding='utf-8') as file:
-    json_places = json.load(file)
-
-with open(vector_data_file_path, 'rt', encoding='utf-8') as file:
-    vector_data = json.load(file)
 
 embeddings = OpenAIEmbeddings(api_key=api_key, model="text-embedding-3-small")
 
@@ -76,12 +76,27 @@ def retrieve_from_vector_database(user_input, user_location, postcode):
     # postcode의 앞 3자리 추출
     postcode3 = postcode[:3]
     print("리트리버 우편 번호 앞 3자리", postcode3)
-    filtered_places = [place for place in json_places if place["postcode"].startswith(postcode3)]
+
+    # 1차 db: postcode가 같은 장소 리스트 저장
+    filtered_places = list(cc_r.find({"postcode": {"$regex": f"^{postcode3}"}}))
+    print("1차 db: ", filtered_places)
+    # 2차 db: 1차 db에서 사용자의 위치에서 가까운 n개 장소 저장
     nearest_place_ids_int = get_nearest_places(user_location, filtered_places, n=13)
-    print(nearest_place_ids_int)
+    print("2차 db: ",nearest_place_ids_int)
     nearest_place_ids = [str(item) for item in nearest_place_ids_int]
-    vector_data_ids = list(vector_data.keys())
-    small_db = {place_id: vector_data[place_id]["embedding"] for place_id in nearest_place_ids if place_id in vector_data_ids}
+
+    vector_data_ids = []
+    cc_v_document = cc_v.find()
+    for doc in cc_v_document:
+        vector_data_ids.extend([key for key in doc.keys() if key.isdigit()])
+
+    small_db = {}
+    for num_id in nearest_place_ids:
+        document = cc_v.find_one({num_id: {"$exists": True}}, {'_id': 0})
+        if document:
+            small_db[num_id] = document[num_id]["embedding"]
+        else:
+            print(f"No document found for id {num_id}")
 
     # 사용자 입력에 가장 가까운 장소 찾기
     k = 5
@@ -101,19 +116,19 @@ def retrieve_from_vector_database(user_input, user_location, postcode):
 
     for place_id in top_places:
         if place_id:
-            similar_place = next((p for p in json_places if str(p["id"]) == str(place_id)), None)
+            similar_place = cc_r.find_one({"id": place_id})
             if similar_place:
                 most_similar_places.append(similar_place)
 
     context = ""
     if most_similar_places:
         for i, place in enumerate(most_similar_places, 1):
-            name = place["place_name"]
-            type = place["category_name"]
-            address = place["road_address_name"]
-            rating = place["rating"]
-            details = ", ".join(place["detail"])
-            phone = place["phone"]
+            name = place.get("place_name", "Unknown")
+            type = place.get("category_name", "Unknown")
+            address = place.get("road_address_name", "Unknown")
+            rating = place.get("rating", "Unknown")
+            details = ", ".join(place.get("detail", []))
+            phone = place.get("phone", "Unknown")
             context += f'''장소 이름은 '{name}'고 장소 타입은 '{type}'이다. 
     데이터베이스 내의 장소 중 현재 위치와 {i}번째로 가까운 장소이며, 주소는 '{address}'이고 평점은 '{rating}'점이다. 
     세부사항으로는 '{details}' 등이 있다.\n'''
